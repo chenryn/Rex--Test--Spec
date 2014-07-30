25b525f6bc5aa25adf728d99abc777def5c76376
饶琛琳 <raochenlin@raochenlindeMacBook-Air.local>
2014-07-30 18:42:02 +0800

    Change license to apache license v2.

--- lib/Rex/Test/Spec.pm
+++ lib/Rex/Test/Spec.pm
@@ -19,11 +19,11 @@ Rex::Test::Spec - Write Rex::Test like RSpec!
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
@@ -215,11 +215,17 @@ Also accept pull requests and issue at L<https://github.com/chenryn/Rex--Test--S
 
 Copyright 2014 Rao Chenlin(chenryn).
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-See L<http://dev.perl.org/licenses/> for more information.
+L<http://www.apache.org/licenses/LICENSE-2.0/>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =cut
 
